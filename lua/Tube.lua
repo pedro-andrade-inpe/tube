01c9df111e188ef9453c1522db70da84ce5d865d
Pedro Andrade <pedro.andrade@inpe.br>
2015-03-06 09:39:11 -0300

    Changing from choice to Choice.

--- lua/Tube.lua
+++ lua/Tube.lua
@@ -6,10 +6,10 @@
 -- @arg data.observingStep Time interval to observe the amount of water along the simulation.
 -- @arg data.checkZero Deny having a negative amount of water in the model.
 Tube = Model{
-	initialWater    = choice{min = 10, default = 200},
-	finalTime       = choice{min = 1, default = 10},
-	flow            = choice{min = 1, default = 20},
-	observingStep   = choice{min = 0.1, max = 1, step = 0.1, default = 1},
+	initialWater    = Choice{min = 10, default = 200},
+	finalTime       = Choice{min = 1, default = 10},
+	flow            = Choice{min = 1, default = 20},
+	observingStep   = Choice{min = 0.1, max = 1, step = 0.1, default = 1},
 	checkZero       = false,
 	init = function(model)
 		model.cell  = TubeCell(model)
@@ -17,7 +17,7 @@ Tube = Model{
 		model.chart = TubeChart(model)
 	end,
 	interface = function()
-		return {{"choice", "boolean"}}
+		return {{"Choice", "boolean"}}
 	end
 }
 
